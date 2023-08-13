50ac318e8bb7ec6dd066a973994d80f8d736b833
Yongha <loha31707@gmail.com>
2023-08-14 03:13:17 +0900

    이동 버튼 개발중

--- Assets/Scripts/MovableButton.cs
+++ Assets/Scripts/MovableButton.cs
@@ -1,11 +1,101 @@
-using UnityEngine.UI;
+using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
 
-/// <summary>
-///
-/// </summary>
-public class MovableButton : Button
+public enum MoveDirType
 {
-    public bool IsPress => base.IsPressed();
-    
-    
+    Up,
+    RightUp,
+    Right,
+    RightDown,
+    Down,
+    LeftDown,
+    Left,
+    LeftUp
+}
+
+public class MovableButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public MoveDirType MoveDirType;
+
+    public bool IsPress { get; private set; }
+
+    public event Action OnPointEnterAction = () => { };
+
+    [SerializeField]
+    private Vector2 moveDirection;
+
+    [Tooltip("민감도")]
+    public float sensitivity = 3f;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        print(MoveDirType);
+
+
+        IsPress = true;
+
+        OnPointEnterAction();
+    }
+
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        IsPress = false;
+    }
+
+    private void FixedUpdate()
+    {
+        if (IsPress == false)
+            return;
+
+        var h = 0f;
+        var v = 0f;
+
+        switch (MoveDirType)
+        {
+            case MoveDirType.Up:
+                v = 1f;
+                break;
+            case MoveDirType.RightUp:
+                v = h = 1f;
+                break;
+            case MoveDirType.Right:
+                h = 1f;
+                break;
+            case MoveDirType.RightDown:
+                v = -1f;
+                h = 1f;
+                break;
+            case MoveDirType.Down:
+                v = -1f;
+                break;
+            case MoveDirType.LeftDown:
+                v = h = -1f;
+                break;
+            case MoveDirType.Left:
+                v = -1f;
+                break;
+            case MoveDirType.LeftUp:
+                v = 1f;
+                h = -1f;
+                break;
+        }
+
+        moveDirection = SmoothInput(h, v);
+    }
+
+
+    private Vector2 SmoothInput(float targetH, float targetV)
+    {
+        var deadZone = 0.001f;
+        var smoothDelta = sensitivity * Time.deltaTime;
+
+        moveDirection.x = Mathf.MoveTowards(moveDirection.x, targetH, smoothDelta);
+        moveDirection.y = Mathf.MoveTowards(moveDirection.y, targetV, smoothDelta);
+
+        return new Vector2(
+            Mathf.Abs(moveDirection.x) < deadZone ? 0f : moveDirection.x,
+            Mathf.Abs(moveDirection.y) < deadZone ? 0f : moveDirection.y);
+    }
 }
--- Assets/Scripts/PlayerUI.cs
+++ Assets/Scripts/PlayerUI.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -17,43 +18,5 @@ public class PlayerUI : MonoBehaviour
     [SerializeField]
     private MovableButton moveRightButton;
 
-    [SerializeField]
-    private Vector2 moveDiretion;
-    
-    public float sensitivity;
 
-    private void FixedUpdate()
-    {
-        float h = 0f;
-        float v = 0f;
-
-        if (moveUpButton.IsPress)
-            v = 1f;
-        else if (moveDownButton.IsPress)
-            v = -1f;
-
-        if (moveLeftButton.IsPress)
-            h = -1f;
-        else if (moveRightButton.IsPress)
-            h = 1f;
-
-        moveDiretion = SmoothInput(h, v);
-    }
-
-
-    private Vector2 SmoothInput(float targetH, float targetV)
-    {
-        sensitivity = 3f;
-        float deadZone = 0.001f;
-
-        moveDiretion.x = Mathf.MoveTowards(moveDiretion.x,
-            targetH, sensitivity * Time.deltaTime);
-
-        moveDiretion.y = Mathf.MoveTowards(moveDiretion.y,
-            targetV, sensitivity * Time.deltaTime);
-
-        return new Vector2(
-            (Mathf.Abs(moveDiretion.x) < deadZone) ? 0f : moveDiretion.x,
-            (Mathf.Abs(moveDiretion.y) < deadZone) ? 0f : moveDiretion.y);
-    }
 }
