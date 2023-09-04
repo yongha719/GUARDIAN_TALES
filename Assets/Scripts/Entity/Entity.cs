4eeef055845a9e2c8342a19b6c411fc98c55d078
yhoney <chaeyh04@gmail.com>
2023-09-05 00:24:14 +0900

    modify damage 수정
    - Dictionary에 속성 데미지 multiplier 정보 저장

--- Assets/Scripts/Entity/Entity.cs
+++ Assets/Scripts/Entity/Entity.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public abstract class Entity : MonoBehaviour
@@ -8,17 +9,41 @@ public abstract class Entity : MonoBehaviour
     [Header(nameof(Entity))]
     private int bbracjji;
     
+    private Dictionary<ElementalAttribute, Dictionary<ElementalAttribute, float>> elementalDamageMultiplier = new Dictionary<ElementalAttribute, Dictionary<ElementalAttribute, float>>();
+
     public abstract EntityData Data { get; set; }
 
     
     protected virtual void Start()
     {
         print(Data.ElementalAttribute);
+        Add(ElementalAttribute.Fire, ElementalAttribute.Water, true);
+        Add(ElementalAttribute.Fire, ElementalAttribute.Earth, false);
+
+        Add(ElementalAttribute.Water, ElementalAttribute.Earth, true);
+        Add(ElementalAttribute.Water, ElementalAttribute.Fire, false);
+
+        Add(ElementalAttribute.Earth, ElementalAttribute.Fire, true);
+        Add(ElementalAttribute.Earth, ElementalAttribute.Water, false);
+
+        Add(ElementalAttribute.Light, ElementalAttribute.Neutral, true);
+        Add(ElementalAttribute.Light, ElementalAttribute.Dark, false);
+
+        Add(ElementalAttribute.Dark, ElementalAttribute.Light, true);
+        Add(ElementalAttribute.Dark, ElementalAttribute.Neutral, false);
+
+        Add(ElementalAttribute.Neutral, ElementalAttribute.Dark, true);
+        Add(ElementalAttribute.Neutral, ElementalAttribute.Light, false);
     }
 
     public virtual void Hit(int damage, ElementalAttribute enemyElemental)
     {
         Data.Hp.Value -= ModifyDamage(damage, enemyElemental);
+    }
+
+    private void Add(ElementalAttribute my, ElementalAttribute other, bool isStrong)
+    {
+        elementalDamageMultiplier[my][other] = isStrong ? 1.3f : 0.7f;
     }
 
     /// <summary>
@@ -29,60 +54,9 @@ public abstract class Entity : MonoBehaviour
     // 유리한 속성일 경우 130% 불리한 경우 70%임
     public virtual int ModifyDamage(int damage, ElementalAttribute enemyElemental)
     {
-        var damageMultiplier = 1.0f;
+        if (!elementalDamageMultiplier.Keys.Contains(Data.ElementalAttribute)) return 1;
+        if (!elementalDamageMultiplier[Data.ElementalAttribute].Keys.Contains(enemyElemental)) return 1;
 
-        switch (Data.ElementalAttribute)
-        {
-            case ElementalAttribute.Fire:
-                damageMultiplier = enemyElemental switch
-                {
-                    ElementalAttribute.Water => 1.3f,
-                    ElementalAttribute.Earth => 0.7f,
-                    _ => 1.0f
-                };
-                break;
-            case ElementalAttribute.Water:
-                damageMultiplier = enemyElemental switch
-                {
-                    ElementalAttribute.Earth => 1.3f,
-                    ElementalAttribute.Fire => 0.7f,
-                    _ => 1.0f
-                };
-                break;
-            case ElementalAttribute.Earth:
-                damageMultiplier = enemyElemental switch
-                {
-                    ElementalAttribute.Fire => 1.3f,
-                    ElementalAttribute.Water => 0.7f,
-                    _ => 1.0f
-                };
-                break;
-            case ElementalAttribute.Light:
-                damageMultiplier = enemyElemental switch
-                {
-                    ElementalAttribute.Neutral => 1.3f,
-                    ElementalAttribute.Dark => 0.7f,
-                    _ => 1.0f
-                };
-                break;
-            case ElementalAttribute.Dark:
-                damageMultiplier = enemyElemental switch
-                {
-                    ElementalAttribute.Light => 1.3f,
-                    ElementalAttribute.Neutral => 0.7f,
-                    _ => 1.0f
-                };
-                break;
-            case ElementalAttribute.Neutral:
-                damageMultiplier = enemyElemental switch
-                {
-                    ElementalAttribute.Dark => 1.3f,
-                    ElementalAttribute.Light => 0.7f,
-                    _ => 1.0f
-                };
-                break;
-        }
-
-        return (int)(damage * damageMultiplier);
+        return (int)(damage * elementalDamageMultiplier[Data.ElementalAttribute][enemyElemental]);
     }
 }
